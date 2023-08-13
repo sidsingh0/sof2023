3036f8e92e08eb48ea9a486fe9c98e086d75f0db
sidsingh0 <sidsinghcs@gmail.com>
2023-08-13 16:18:03 +0530

    .

--- admin/allotment.php
+++ admin/allotment.php
@@ -5,13 +5,8 @@
         $company_id = $_POST["company_id"];
         $student_id = $_POST["student_id"];
 
-        if($attended == 1){
-            $attended = 0;
-        }else{
-            $attended = 1;
-        }
 
-        $query = "update students set attended=$attended where phone=$phone";
+        $query = "insert into allotments (student_id,company_id) VALUES ('$student_id','$company_id')";
         $res = mysqli_query($conn, $query);
 
         if($res){
