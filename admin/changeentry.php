52eaf07e194f799dba3a41e7c1da3f79d7ffa036
sidsingh0 <sidsinghcs@gmail.com>
2023-08-15 18:38:23 +0530

    delete

--- admin/changeentry.php
+++ admin/changeentry.php
@@ -1,10 +1,19 @@
-<?php include("./connect.php");
+<?php 
+include("./connect.php");
 
-if ((isset($_POST["student_id"])) and (isset($_POST["company_id"]))) {
-    $student_id=$_POST["student_id"];
-    $company_id=$_POST["company_id"];
-    $query="delete from allotments where student_id='$student_id' and company_id='$company_id'";
-    $query_res=mysqli_query($conn,$query);
-    echo 1;
+if (isset($_POST["student_id"]) && isset($_POST["company_id"])) {
+    $student_id = $_POST["student_id"];
+    $company_id = $_POST["company_id"];
+
+    $query = "DELETE FROM allotments WHERE student_id = '$student_id' AND company_id = '$company_id'";
+    $query_res = mysqli_query($conn, $query);
+
+    if ($query_res) {
+        echo "Record deleted successfully.";
+    } else {
+        echo "Error deleting record: " . mysqli_error($conn);
+    }
+} else {
+    echo "Both student ID and company ID are required.";
 }
 ?>
