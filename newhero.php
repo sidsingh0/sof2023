a23ac426b39208bc73d9b20e2d6d145a73b10dea
sidsingh0 <sidsinghcs@gmail.com>
2023-08-25 00:42:39 +0530

    ...

--- newhero.php
+++ newhero.php
@@ -259,7 +259,11 @@ include("./connect.php");
     </div>
   </footer>
   <!-- End Footer -->
+  <section></section>
 
+  <footer class="newfooter">
+
+  </footer>
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
